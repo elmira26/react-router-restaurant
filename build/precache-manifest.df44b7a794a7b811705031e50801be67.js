7bcec41101fdd2dd4b422b6832a0b4af02af3aca
lle <e-ve-j@hotmail.com>
2020-11-30 22:52:11 +0300

    commit

---
+++ build/precache-manifest.df44b7a794a7b811705031e50801be67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06445df321a3560ee037b7705e73b0a8",
+    "url": "/restaurant-react-app/index.html"
+  },
+  {
+    "revision": "85a3ddc6e36a0cefde51",
+    "url": "/restaurant-react-app/static/css/2.53194f49.chunk.css"
+  },
+  {
+    "revision": "3ff8cd77286b783ee0a7",
+    "url": "/restaurant-react-app/static/css/main.8b8735b9.chunk.css"
+  },
+  {
+    "revision": "85a3ddc6e36a0cefde51",
+    "url": "/restaurant-react-app/static/js/2.535426dd.chunk.js"
+  },
+  {
+    "revision": "10c32405ee75161f0d737d3fab388ca8",
+    "url": "/restaurant-react-app/static/js/2.535426dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff8cd77286b783ee0a7",
+    "url": "/restaurant-react-app/static/js/main.bb4ca151.chunk.js"
+  },
+  {
+    "revision": "f693d9f4351a0e59e219",
+    "url": "/restaurant-react-app/static/js/runtime-main.35050822.js"
+  },
+  {
+    "revision": "25fe64fb1c77b3e2cbfd7b9288cfd7d3",
+    "url": "/restaurant-react-app/static/media/jason-leung-unsplash.25fe64fb.jpg"
+  },
+  {
+    "revision": "5820d7194501244f8dd0c2f6e1d44e20",
+    "url": "/restaurant-react-app/static/media/order.5820d719.jpg"
+  },
+  {
+    "revision": "dd1167cf3db2b616186d12ffff674a8f",
+    "url": "/restaurant-react-app/static/media/priscilla-du-preez-unsplash.dd1167cf.jpg"
+  }
+]);
