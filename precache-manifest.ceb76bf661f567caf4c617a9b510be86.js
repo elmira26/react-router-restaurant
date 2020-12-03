b43bd3cab21df9206bb733c780bfbe9076f61c4a
lle <e-ve-j@hotmail.com>
2020-12-03 15:36:48 +0300

    Updates

---
+++ precache-manifest.ceb76bf661f567caf4c617a9b510be86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0ea4474f0b2410c1e1ab3ccf99d148",
+    "url": "/react-router-restaurant/index.html"
+  },
+  {
+    "revision": "e68b45343356fe034300",
+    "url": "/react-router-restaurant/static/css/2.53194f49.chunk.css"
+  },
+  {
+    "revision": "6269efdc64e3ae3a0d14",
+    "url": "/react-router-restaurant/static/css/main.4e94e534.chunk.css"
+  },
+  {
+    "revision": "e68b45343356fe034300",
+    "url": "/react-router-restaurant/static/js/2.fdb5c2ee.chunk.js"
+  },
+  {
+    "revision": "10c32405ee75161f0d737d3fab388ca8",
+    "url": "/react-router-restaurant/static/js/2.fdb5c2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6269efdc64e3ae3a0d14",
+    "url": "/react-router-restaurant/static/js/main.35ab8a38.chunk.js"
+  },
+  {
+    "revision": "8b1eaa3a631114027910",
+    "url": "/react-router-restaurant/static/js/runtime-main.e83b3d84.js"
+  },
+  {
+    "revision": "25fe64fb1c77b3e2cbfd7b9288cfd7d3",
+    "url": "/react-router-restaurant/static/media/jason-leung-unsplash.25fe64fb.jpg"
+  },
+  {
+    "revision": "5820d7194501244f8dd0c2f6e1d44e20",
+    "url": "/react-router-restaurant/static/media/order.5820d719.jpg"
+  },
+  {
+    "revision": "dd1167cf3db2b616186d12ffff674a8f",
+    "url": "/react-router-restaurant/static/media/priscilla-du-preez-unsplash.dd1167cf.jpg"
+  }
+]);
